6d723eff9ff66ff648f25a3fe67245579bbf6d89
Peter Piekarczyk <peterpiekarczyk@gmail.com>
2019-06-02 19:45:54 -0500

    smaller footer image

--- src/layouts/footer.js
+++ src/layouts/footer.js
@@ -6,7 +6,7 @@ export default ({ title, cfpLink }) => (
   <footer className="border-top border-light pt-4 mt-4">
     <ul className="container list-unstyled list-inline text-center">
       <li className="list-inline-item mr-4">
-        <Logo width={240} />
+        <Logo width={140} />
       </li>
       <li className="list-inline-item p-2">
         <Link to="/sponsors">Sponsor</Link>
